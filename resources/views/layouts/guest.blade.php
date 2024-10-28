ff48ec9e3965898719522bb26d0862a1cb4229e6
JoseSacra <jose.sacra.isc@hotmail.com>
2024-10-27 20:05:32 -0600

    Actualizacion JS

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,8 +18,9 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,400;1,500;1,600&family=Roboto+Slab:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
 
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css" />
         <link rel="stylesheet" href="./css/slick.css"/>
-        <link rel="stylesheet" href="./css/tailwind.css"/>
+{{--         <link rel="stylesheet" href="./css/tailwind.css"/> --}}
         <link rel="stylesheet" href="./css/custom.css" />
 
         <!-- Scripts -->
@@ -346,11 +347,13 @@
         </div>
 
         <!--==== js =====-->
-        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-        <script src="{{ asset('js/jquary.min.js') }}"></script>
+
+       <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
+
+        <!-- jQuery Modal -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js"></script>
         <script src="{{ asset('js/slick.js') }}"></script>
         <script src="{{ asset('js/isotope.pkgd.min.js') }}"></script>
-        <script src="{{ asset('js/jquery.modal.min.js') }}"></script>
         <!-- main js -->
         <script src="{{ asset('js/main.js') }}"></script>
 
